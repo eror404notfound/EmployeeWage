c19b7b0a4daa6c6c65cff87977c4b117f8293dca
Ankit <ankitcool4370@gmail.com>
2022-11-10 16:10:18 +0530

    EmployeeAbsent

--- EmoloyeeWage/Program.cs
+++ EmoloyeeWage/Program.cs
@@ -11,6 +11,21 @@ namespace EmoloyeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage");
+            int IS_FULL_TIME = 1;
+           
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+               
+
+            }
+            Console.ReadLine();
 
         }
     }
