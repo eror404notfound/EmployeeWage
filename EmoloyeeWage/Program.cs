aa9e6ccc0e6f3a20cec8215d9c872c70343dc8f6
Ankit <ankitcool4370@gmail.com>
2022-11-10 20:31:04 +0530

    EmoloyeeWagePartTime

--- EmoloyeeWage/Program.cs
+++ EmoloyeeWage/Program.cs
@@ -10,15 +10,20 @@ namespace EmoloyeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Wage Calculation");
+            Console.WriteLine("Welcome to the Employee Wage Part Time");
 
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
@@ -28,8 +33,6 @@ namespace EmoloyeeWage
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
-
-
 
             Console.ReadLine();
 
