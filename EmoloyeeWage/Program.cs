881df1122fec84ff9f9558a15ff01233dc587479
Ankit <ankitcool4370@gmail.com>
2022-11-10 20:49:29 +0530

    EmployeeWagePerMonth

--- EmoloyeeWage/Program.cs
+++ EmoloyeeWage/Program.cs
@@ -9,33 +9,46 @@ namespace EmoloyeeWage
    
     internal class Program
     {
-        
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUS = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
+
+        public static int computeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empCheck = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUS;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            return totalEmpWage;
+
+        }
         static void Main(string[] args)
         {
-           
-
-            int empHrs = 0;
-            int empWage = 0;
-            Random rand = new Random();
-
-            int empCheck = rand.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empCheck = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUS;
-            Console.WriteLine("Emp Wage : " + empWage);
+            computeEmpWage();
 
             Console.ReadLine();
 
