4a7f1881688d622d948cec6f7ae640e6200b735d
Ankit <ankitcool4370@gmail.com>
2022-11-10 20:17:21 +0530

    EmployeeWageCalculation

--- EmoloyeeWage/Program.cs
+++ EmoloyeeWage/Program.cs
@@ -10,21 +10,27 @@ namespace EmoloyeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Wage");
+            Console.WriteLine("Welcome to the Employee Wage Calculation");
+
             int IS_FULL_TIME = 1;
-           
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
-               
-
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+
+
+
             Console.ReadLine();
 
         }
