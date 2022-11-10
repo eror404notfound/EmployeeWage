f8325ef3031684a272a0a5d553b364db20e9e000
Ankit <ankitcool4370@gmail.com>
2022-11-10 20:42:22 +0530

    SwitchCase

--- EmoloyeeWage/Program.cs
+++ EmoloyeeWage/Program.cs
@@ -6,32 +6,35 @@ using System.Threading.Tasks;
 
 namespace EmoloyeeWage
 {
+   
     internal class Program
     {
+        
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUS = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Wage Part Time");
+           
 
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            Random rand = new Random();
+
+            int empCheck = rand.Next(0, 3);
+            switch (empCheck)
             {
-                empHrs = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empCheck = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
+            empWage = empHrs * EMP_RATE_PER_HOUS;
             Console.WriteLine("Emp Wage : " + empWage);
 
             Console.ReadLine();
