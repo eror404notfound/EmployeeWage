4080da432c6554335b21fa2eacf4fc0e828888c3
Ankit <ankitcool4370@gmail.com>
2022-11-25 21:49:22 +0530

    Implement UC case 12

---
+++ EmoloyeeWage/IComputeEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmoloyeeWage
+{
+    internal class IComputeEmpWage
+    {
+        public void AddCompanyEmpWage(string company, int empRateperHour, int numOfWorkingDays, int maxHoursPermonth);
+        public void ComputeEmpWage();
+    }
+}
